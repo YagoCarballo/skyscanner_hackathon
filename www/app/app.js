0dbb61e20dcaae8d16958f66a352bf4a144db15d
Yago Carballo Porto <yagocarballo@gmail.com>
2014-11-08 14:48:21 +0000

    added configuration

--- www/app/app.js
+++ www/app/app.js
@@ -1,96 +1,85 @@
 //Create the module
-var client = angular.module('social-flights', ['ngRoute', 'ngCookies', 'ui.bootstrap', 'ngMaterial', 'social-flights.controllers', 'social-flights.query', 'social-flights.register']);
+angular.module('social-flights', [
+  'ngRoute', 'ngCookies', 'ui.bootstrap', 'ngMaterial',
 
-//Configure routes
-client.config(function($routeProvider, $httpProvider){
+  // Configuration
+  'social-flights',
 
- $httpProvider.defaults.useXDomain = true;
- $httpProvider.defaults.withCredentials = true;
+  // Controllers
+  'social-flights.controllers', 'social-flights.register'
+])
 
-  $httpProvider.interceptors.push('authInterceptor');
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/home.html',
-      controller: 'appController',
-      requiresLogin: true
-    })
-    .when('/about', {
-      templateUrl: 'views/about.html',
-      controller: 'aboutController',
-      security: true
-    })
-    .when('/contact', {
-      templateUrl: 'views/contact.html',
-      controller: 'contactController',
-      requiresLogin: true
-    })
-    .when('/login', {
-      templateUrl: 'views/login.html',
-      controller: 'loginController',
-      requiresLogin: true
-    })
-    .when('/profile', {
-      templateUrl: 'views/profile.html',
-      controller: 'profileController',
-      requiresLogin: true
-    })
-    .when('/controlpanel', {
-      templateUrl: 'views/controlpanel.html',
-      controller: 'cpController',
-      security: true
-    })
-    .when('/register', {
-      templateUrl: 'views/register.html',
-      controller: 'registerController',
-      requiresLogin: true
-    })
-    .when('/countries', {
-      templateUrl: 'views/countries.html',
-      controller: 'countriesController'
-    })
-    .when('/logout', {
-      templateUrl: 'views/home.html',
-      controller: 'logoutController'
-    })
+  //Configure routes
+  .config(function($routeProvider, $httpProvider){
 
-  // if none of the above states are matched, use this as the fallback
-  .otherwise('/');
-});
+   $httpProvider.defaults.useXDomain = true;
+   $httpProvider.defaults.withCredentials = true;
 
+    $httpProvider.interceptors.push('authInterceptor');
+    $routeProvider
 
-//Auth
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'appController',
+        requiresLogin: true
+      })
 
+      // Login
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'loginController',
+        requiresLogin: true
+      })
 
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'registerController',
+        requiresLogin: true
+      })
 
-client.factory('authInterceptor', function ($rootScope, $q, $window) {
-  return {
-    request: function (config) {
-      config.headers = config.headers || {};
-      if ($window.sessionStorage.token) {
-        config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+      .when('/profile', {
+        templateUrl: 'views/profile.html',
+        controller: 'profileController',
+        requiresLogin: true
+      })
+
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'aboutController',
+        security: true
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'contactController',
+        requiresLogin: true
+      })
+
+      .when('/countries', {
+        templateUrl: 'views/countries.html',
+        controller: 'countriesController'
+      })
+
+    // if none of the above states are matched, use this as the fallback
+    .otherwise('/');
+  })
+
+  .factory('authInterceptor', function ($rootScope, $q, $window) {
+    return {
+      request: function (config) {
+        config.headers = config.headers || {};
+        if ($window.sessionStorage.token) {
+          config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+        }
+        return config;
+      },
+      response: function (response) {
+        if (response.status === 401) {
+          // handle the case where the user is not authenticated
+        }
+        return response || $q.when(response);
       }
-      return config;
-    },
-    response: function (response) {
-      if (response.status === 401) {
-        // handle the case where the user is not authenticated
-      }
-      return response || $q.when(response);
-    }
-  };
-});
-
-
-
-
-
-//Directives
-
-
-
-//Controllers
-
-
+    };
+  });
 
 //Handles the user auth
 function checkAuth(cookie)  {
