bf95e21ae763186069bf73ee36b0ce1c8967362a
Yago Carballo Porto <yagocarballo@gmail.com>
2014-11-09 09:37:46 +0000

    Revert "group"
    This reverts commit 094e269c41339b3ff935834c9333b43c6c275024.

--- www/app/controllers/group.js
+++ www/app/controllers/group.js
@@ -38,48 +38,4 @@ angular.module('social-flights.controllers.group', ['ngRoute', 'ngCookies', 'ui.
         $scope.openProfile = function (member) {
             $location.path("/profile/"+member.id);
         };
-
-        $scope.flightDetails = function (flight) {
-            $location.path("/flight/"+flight.id);
-        };
-
-        $scope.findFlights = function () {
-            $location.path("/flights");
-        };
-
-        fetchFlights($scope, $http, $location);
     });
-
-function fetchFlights ($scope, $http, $location) {
-    $http({
-        url: backend+"/groups/"+$scope.group_id+'/flights',
-        method: 'GET',
-        dataType: 'json',
-        data: {},
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            'Authorization': 'SOCIALFLIGHT '+localStorage.getItem('access_token')
-        }
-    }).success(function (data, status, headers, config) {
-        console.log(data);
-
-        if(data.status_code == "200"){
-            $scope.flights = data.data;
-            localStorage.setItem('flights', JSON.stringify(data.data));
-
-        } else if(data.status_code == "403") {
-            $scope.registerError = data.status_code;
-
-            localStorage.removeItem('access_token');
-            localStorage.removeItem('user');
-
-            $location.path("/login");
-        }
-
-        console.log(data.status_code);
-
-    }).error(function(data, status, headers, config) {
-        $scope.registerStatus = status;
-        console.log("error");
-    });
-}
