067d5b5d3d59b5914f508baf8e8fd39bab744049
Yago Carballo Porto <yagocarballo@gmail.com>
2014-11-09 05:40:19 +0000

    added flights logic

--- www/app/controllers/flights.js
+++ www/app/controllers/flights.js
@@ -5,6 +5,13 @@ angular.module('social-flights.controllers.flights', ['ngRoute', 'ngCookies', 'u
         $scope.selected = undefined;
         $scope.countries = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
+        $scope.form = {
+            from: 'EDI',
+            to: 'LHR',
+            inbound: (12).days().fromNow(),
+            outbound: (7).days().fromNow()
+        };
+
         $scope.search = function () {
             $http({
                 url: backend+"/session/create",
@@ -15,11 +22,11 @@ angular.module('social-flights.controllers.flights', ['ngRoute', 'ngCookies', 'u
                     country: 'GB',
                     currency: 'GBP',
                     locale: 'en-GB',
-                    originplace: 'EDI',
-                    destinationplace: 'LHR',
-                    outbounddate: '2014-11-11',
-                    inbounddate: '2014-11-20',
-                    adults: 1,
+                    originplace: $scope.form.from,
+                    destinationplace: $scope.form.to,
+                    outbounddate: $scope.form.inbound,
+                    inbounddate: $scope.form.outbound,
+                    adults: $scope.people,
                     children: 1,
                     locationschema: 'lata'
                 }),
@@ -52,18 +59,6 @@ angular.module('social-flights.controllers.flights', ['ngRoute', 'ngCookies', 'u
         };
     });
 
-function formEncode(obj) {
-    var encodedString = '';
-    for (var key in obj) {
-        if (encodedString.length !== 0) {
-            encodedString += '&';
-        }
-
-        encodedString += key + '=' + encodeURIComponent(obj[key]);
-    }
-    return encodedString.replace(/%20/g, '+');
-}
-
 function findFlights ($scope, $http, $location) {
     $http({
         url: backend + "/session/poll/" + $scope.data_location.location.replace('http://partners.api.skyscanner.net/apiservices/pricing/v1.0/', ''),
@@ -77,8 +72,8 @@ function findFlights ($scope, $http, $location) {
     }).success(function (data, status, headers, config) {
         $scope.loading = false;
 
-        $scope.flights = data.data;
-        console.log(data);
+        $scope.flights = data;
+        parseFlights($scope, data);
 
     }).error(function (data, status, headers, config) {
         $scope.loading = false;
@@ -87,8 +82,32 @@ function findFlights ($scope, $http, $location) {
     });
 }
 
-function parseFlights (raw) {
-    raw.Itineraries.each(
+function parseFlights ($scope, raw) {
+    $scope.flights = [];
 
-    );
+    raw.Itineraries.forEach(function(route){
+        var flight = {};
+
+        raw.Legs.forEach(function(leg){
+            if (leg.Id === route.InboundLegId) {
+                flight.duration = leg.Duration;
+                if (raw.Places[leg.SegmentIds[0]]) {
+                    flight.from = raw.Places[leg.SegmentIds[0]].Code;
+                }
+                flight.stops = leg.Stops.length;
+
+                raw.Carriers.forEach(function (carrier) {
+                    if (carrier.Id === leg.Carriers[0]) {
+                        flight.carrier = carrier.name;
+                        flight.carrierLogo = carrier.ImageUrl;
+                    }
+                });
+
+            } else if (leg.Id === route.OutboundLegId) {
+                flight.to = raw.Places[leg.SegmentIds[0]].Code;
+            }
+        });
+
+        $scope.flights.push(flight);
+    });
 }
